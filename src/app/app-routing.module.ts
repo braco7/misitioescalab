4437b5a5d75fe36e334da28af8254bf2bd16630e
Francisco Ariza <frajarsa@gmail.com>
2022-03-21 18:26:47 -0300

    Implementación de Guards para proteger las rutas /calculadora y /listado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuardianGuard } from './guards/guardian.guard';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
   { path: '', redirectTo: 'home', pathMatch: "full" },
-  { path: 'calculadora', loadChildren: () => import('./calculadora/calculadora.module').then(m => m.CalculadoraModule) },
+  { path: 'calculadora', canActivate: [GuardianGuard], loadChildren: () => import('./calculadora/calculadora.module').then(m => m.CalculadoraModule) },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'support', loadChildren: () => import('./support/support.module').then(m => m.SupportModule) }
+  { path: 'support', canActivate: [GuardianGuard], loadChildren: () => import('./support/support.module').then(m => m.SupportModule) }
 ];
 
 @NgModule({
